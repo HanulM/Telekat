56e3ffa77c9681cb80ad4a0306ec9110cb2e8b9f
HanulM <hanul.m26@gmail.com>
2020-09-25 14:17:51 -0400

    added method declerations for collisions

--- Telekat/Telekat/Physics.cs
+++ Telekat/Telekat/Physics.cs
@@ -9,13 +9,35 @@ namespace Telekat
     class Physics
     {
         // Attributes
-
+        private bool topCollided;
+        private bool rightCollided;
+        private bool leftCollided;
+        private bool bottomCollided;
 
         // Properties
 
 
         // Methods
         // Collision Detection
+        public bool TopCollision()
+        {
+            topCollided = false;
+        }
+
+        public bool RightCollision()
+        {
+            rightCollided = false;
+        }
+
+        public bool LeftCollision()
+        {
+            leftCollided = false;
+        }
+
+        public bool BottomCollision()
+        {
+            bottomCollided = false;
+        }
 
         // Gravity
     }
