d25d058bd48ee6c091ada760a26fc1fe01a6aefa
maia silverio <maiasilver13@gmail.com>
2020-10-10 14:41:46 -0400

    Changed 'System.Microsoft.-' to 'Microsoft.-'

--- Telekat/Telekat/Items.cs
+++ Telekat/Telekat/Items.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Microsoft.Xna.Framework;
-using System.Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Telekat
 {
