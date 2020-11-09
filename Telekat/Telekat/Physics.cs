95429105f6b19f8bed1495f55576db3bedb30227
HanulM <hanul.m26@gmail.com>
2020-11-08 22:11:15 -0500

    added collision

--- Telekat/Telekat/Physics.cs
+++ Telekat/Telekat/Physics.cs
@@ -3,36 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Telekat
 {
     class Physics
     {
         #region Fields
-
+        protected Rectangle firstRectangle;
+        protected Rectangle secondRectangle;
         #endregion
 
-        // Properties
+        #region properties
+        public Rectangle FirstRectangle { get { return this.firstRectangle; } }
+        public Rectangle SecondRectangle { get { return this.secondRectangle; } }
+        #endregion
 
 
         // Methods
         // Collision Detection
         public bool Collided()
         {
-            /*
-             * if(klaus.characterBox.Contains(myItems.itemBox)
-             * {
-             *      return true;
-             * }
-             */
+            if (FirstRectangle.Contains(SecondRectangle))
+            {
+                return true;
+            }
 
             return false; //else
         }
 
-        // Gravity
-        public void Gravity(Klaus player)
+        // Gravity - Unsure on how to implement
+        /*public void Gravity(Klaus player)
         {
 
-        }
+        }*/
     }
 }
---
+++ Telekat/Telekat/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Telekat
+{
+    class Platform : Items
+    {
+        public override bool ItemActive()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
